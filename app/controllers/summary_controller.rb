947a5aeb23713064e6316598a6a7234091385d12
Shaun Meredith <shaun@infobridge.net>
2009-10-29 09:04:25 -0400

    change summary to show ascending days

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -3,7 +3,7 @@ class SummaryController < ApplicationController
   def index
     @page_title = "Scheduler: Summary for #{current_user.name.capitalize}"
     @tabs = Team.find :all
-    @tides = Tide.find :all, :conditions=> [ "day >= ?", Time.now - 1.day ], :limit => 3
+    @tides = Tide.find :all, :conditions=> [ "day >= ?", Time.now - 1.day ], :order => "day ASC", :limit => 3
     @total_bulletins = Bulletin.count :all, :conditions => ["display_until >= ?", Time.now ]
     @bulletins = Bulletin.find :all, :conditions => ["display_until >= ?", Time.now ], :order=> 'created_at DESC', :limit => 3
     @today = Event.today
