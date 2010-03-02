20192a5d2fce3fd8a8e037ec76e08ef9a30b5c3d
Shaun Meredith <shaun@infobridge.net>
2010-03-02 11:27:34 -0500

    removed calendar action from tide after create

--- app/models/tide.rb
+++ app/models/tide.rb
@@ -1,6 +1,6 @@
 class Tide < ActiveRecord::Base
   validates_presence_of :day
-  after_create :add_to_calendar
+  # after_create :add_to_calendar
   
   def self.import_tide_csv(csv_path, time_zone = nil)
     time_zone = "UTC" if time_zone.blank?
