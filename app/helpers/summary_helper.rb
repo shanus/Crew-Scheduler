d668f3600d3e317dc2803d98101f37d18e4685de
Shaun Meredith <shanus@Baldr.local>
2011-07-20 09:47:48 -0400

    updated maximum rowers calls to check to make sure an event has a boat first

--- app/helpers/summary_helper.rb
+++ app/helpers/summary_helper.rb
@@ -1,19 +1,25 @@
 module SummaryHelper
   def seats_needed(event)
 		seats_needed = ""
-		1.upto(event.boat.max_number_of_rowers) { |i|
-			seat = "#{i}"
-			if (event.boat.max_number_of_rowers==1)
-				seat = "single"
-			elsif (i==event.boat.max_number_of_rowers)
-				seat = "stroke"
-			elsif (i==1)
-				seat = "bow"
-			end 
-			seats_needed = "#{seats_needed}#{seat}, " if event.rowers[i].nil?
-		} 
-		seats_needed = "#{seats_needed}coxswain, " if event.needs_coxswain?
-		seats_needed = "#{seats_needed}coach, " if event.needs_coach?
+		unless event.nil?
+		  max_rowers = 1
+      if (event.boat)
+        max_rowers = event.boat.max_number_of_rowers
+      end
+  		1.upto(max_rowers) { |i|
+  			seat = "#{i}"
+  			if (max_rowers==1)
+  				seat = "single"
+  			elsif (i==max_rowers)
+  				seat = "stroke"
+  			elsif (i==1)
+  				seat = "bow"
+  			end 
+  			seats_needed = "#{seats_needed}#{seat}, " if event.rowers[i].nil?
+  		} 
+  		seats_needed = "#{seats_needed}coxswain, " if event.needs_coxswain?
+  		seats_needed = "#{seats_needed}coach, " if event.needs_coach?
+  	end
 		seats_needed.rstrip.sub(/,$/,'')
   end
 end
--- app/models/seating_position.rb
+++ app/models/seating_position.rb
@@ -3,9 +3,16 @@ class SeatingPosition < ActiveRecord::Base
   belongs_to :user
   
   def self.init(event)
-    1.upto(event.boat.max_number_of_rowers) { |seat|
-      SeatingPosition.create( :event => event, :user => nil, :position => seat)
-    }
+    unless event.nil?
+      max_rowers = 1
+      if (event.boat)
+        max_rowers = event.boat.max_number_of_rowers
+      end
+      1.upto(max_rowers) { |seat|
+        SeatingPosition.create( :event => event, :user => nil, :position => seat)
+      }
+    end
+    self
   end
   
 end
